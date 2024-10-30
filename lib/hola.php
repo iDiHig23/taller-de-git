596c4663f12494b3b40bd7d4bdbd189c9ac7c792
Ismael <idiahig064@g.educaand.es>
2024-10-30 13:12:32 +0100

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,8 +1,6 @@
 <?php
 // Autor: ismael <idiahig064@g.educaand.es>
-// El nombre por defecto es Mundo
 
-require('HolaMundo.php');
-
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print new HolaMundo($nombre);
+print "Introduce tu nombre";
+$nombre = trim(fgets(STDIN));
+@print "Hola, {$nombre}";
